32359e22261abfc53adc763a67c7ee36b99ed8af
Isaac <zhaparovaziz03@gmail.com>
2023-06-07 15:56:52 +0300

    Создание потоков

---
+++ StartCS_Threads/StartCS_Threads/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+
+internal class Program
+{   
+    private static async Task Main(string[] args)
+    {
+        void AsyncThread()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Асинхронный поток {i}\n");
+                Thread.Sleep(500);
+            }
+        }
+
+        void SecondThread(object? name)
+        {
+            Console.WriteLine($"{name} начал работу");
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"* | {name}\n");
+                Thread.Sleep(600);
+            }
+        }
+
+        async Task MainThread()
+        {
+            Thread secondThread = new Thread(new ParameterizedThreadStart(SecondThread));
+            secondThread.Name = "Второй поток";
+            secondThread.Start(secondThread.Name);
+            secondThread.IsBackground = true;
+
+            await Task.Run(() => AsyncThread());
+
+            var mainThread = Thread.CurrentThread;
+            mainThread.Name = "Главный поток";
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"{mainThread.Name}: {i}\n");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine($"{mainThread.Name} завершен");
+            Console.WriteLine($"{secondThread.Name} завершен");
+
+            Console.ReadKey();
+        }
+
+        await MainThread();
+    }
+}
